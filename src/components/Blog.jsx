2b2f042eab95c577823a9d1f96b9c569b1a04469
PabloFC <pablofc_68@hotmail.com>
2024-05-22 13:27:24 +0200

    Blog hardcodeado

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,6 +1,7 @@
 // import grafica from "../assets/img/landing/grafica.png";
-import imgBlogGrande from "../assets/img/landing/imgBlogGrande.png";
-import imgBlogPeq from "../assets/img/landing/imgBlogPeq.png";
+import oro from "../assets/img/landing/oro.jpg";
+import apple from "../assets/img/landing/apple.jpg";
+import petroleo from "../assets/img/landing/petroleo.jpg";
 
 const Blog = () => {
   return (
@@ -11,37 +12,47 @@ const Blog = () => {
       <div className="flex flex-col sm:flex-row mx-auto justify-center">
         <div className="w-full mb-4 basis-1/2 border-2 border-solid rounded-xl">
           <div>
-            <img src={imgBlogGrande} className="w-full " alt="grafica" />
+            <img src={oro} className="w-full " alt="grafica" />
           </div>
           <div className="mb-2 p-8">
             <h3 className="text-primario text-2xl font-bold mt-4 mb-4">
-              Cursos de Inversión
+              El precio del Oro apunta a máximos históricos
             </h3>
             <p className="text-xl">
-              Imperia secures €3M funding led by Samaipata for its innovative
-              SaaS supply chain solution. Backed by top investors, Imperiaaims
-              for Spanish market consolidation and international expansion....
+              El precio del Oro se disparó durante la sesión norteamericana
+              previa al fin de semana, ya que el par XAU/USD cotizó por encima
+              de 2.400$, registrando ganancias de más del 1.5% en medio de la
+              subida de los rendimientos de los bonos del Tesoro estadounidense.
+              El metal sin rendimiento amplió su avance y amenazó con romper el
+              máximo histórico de 2.431$...
             </p>
           </div>
           <div className="flex justify-end mr-4 py-4">
-            <button className="bg-secundario px-6 py-2 text-white mt-4 rounded-3xl ">
-              Leer más
-            </button>
+            <a href="https://www.fxstreet.es/news/el-precio-del-oro-supera-el-nivel-2400-apunta-a-maximos-historicos-202405172003">
+              <button className="bg-secundario px-6 py-2 text-white mt-2 rounded-3xl ">
+                Leer más
+              </button>
+            </a>
           </div>
         </div>
 
         <div className="w-full basis-1/2 xl:ml-8 ">
-          <div className="mb-5 w-full basis-1/2 xl:py-4 border-2 border-solid rounded-xl">
+          <div className="mb-5 w-full basis-1/2  border-2 border-solid rounded-xl">
             <div className="mb-3">
-              <img src={imgBlogPeq} className="w-full" alt="grafica" />
+              <img
+                src={apple}
+                className="w-full xl:h-[200px]  "
+                alt="grafica"
+              />
             </div>
             <div className="p-5">
-              <h3 className="text-primario text-2xl font-bold mb-2">
-                Cursos de Inversión
+              <h3 className="text-primario text-2xl xl:text-xl font-bold mb-2">
+                Apple nombrada la mejor empresa estadounidense en 2024
               </h3>
-              <p className="text-xl xxl:pr-16 md:pr-0">
-                Imperia secures €3M funding led by Samaipata for its innovative
-                SaaS supply chain solution...
+              <p className="text-xl xxl:pr-16 xl:text-lg md:pr-0">
+                Apple ha sido nombrada la mejor empresa estadounidense en 2024
+                por su innovación continua, fuerte desempeño financiero y
+                liderazgo en sostenibilidad...
               </p>
               <div className="flex justify-end text-secundario text-md hover:cursor-pointer mr-4">
                 <p className="mr-12 lg:mr-0 mt-1">Leer más</p>
@@ -49,17 +60,22 @@ const Blog = () => {
             </div>
           </div>
 
-          <div className="mb-5 w-full basis-1/2 xl:py-4 border-2 border-solid rounded-xl">
+          <div className="mb-5 w-full basis-1/2  border-2 border-solid rounded-xl">
             <div className="mb-3">
-              <img src={imgBlogPeq} className="w-full" alt="grafica" />
+              <img
+                src={petroleo}
+                className="w-full xl:h-[200px]"
+                alt="grafica"
+              />
             </div>
             <div className="p-5">
-              <h3 className="text-primario text-2xl font-bold mb-2">
-                Cursos de Inversión
+              <h3 className="text-primario text-2xl xl:text-xl font-bold mb-2">
+                El precio del petróleo sube por tensiones en Medio Oriente
               </h3>
-              <p className="text-xl xxl:pr-16 md:pr-0">
-                Imperia secures €3M funding led by Samaipata for its innovative
-                SaaS supply chain solution...
+              <p className="text-xl xl:text-lg xxl:pr-16 md:pr-0">
+                El precio del petróleo crudo subió un 5% hoy debido a las
+                crecientes tensiones geopolíticas en Medio Oriente, que han
+                afectado la oferta mundial...
               </p>
               <div className="flex justify-end text-secundario text-md hover:cursor-pointer mr-4">
                 <p className="mr-12 lg:mr-0 mt-1">Leer más</p>
