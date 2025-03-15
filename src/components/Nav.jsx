2ac7c273eccfcff3d8a60f636b88dfa2f88c2a6a
PabloFC <pablofc_68@hotmail.com>
2025-03-15 21:39:49 +0100

    limpiando codigo en el componente nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,12 @@ import { Button, Navbar } from "flowbite-react";
 import logo from "../assets/img/landing/logo.png";
 
 const Nav = () => {
+  const links = [
+    { href: "#sobreNosotros", label: "Sobre Nosotros" },
+    { href: "#blog", label: "Blog" },
+    { href: "#faqs", label: "FAQS" },
+  ];
+
   return (
     <Navbar fluid rounded className="bg-gray-200 xl:sticky xl:top-0">
       <Navbar.Brand className="lg:ml-4 xl:ml-40 w-32">
@@ -20,25 +26,16 @@ const Nav = () => {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link
-          href="#sobreNosotros"
-          className="hover:!text-secundario text-stone-950 text-md font-bold"
-        >
-          Sobre Nosotros
-        </Navbar.Link>
-        <Navbar.Link
-          href="#blog"
-          className="hover:!text-secundario text-stone-950 text-md font-bold"
-        >
-          Blog
-        </Navbar.Link>
+        {links.map((link) => (
+          <Navbar.Link
+            key={link.href}
+            href={link.href}
+            className="hover:!text-secundario text-stone-950 text-md font-bold"
+          >
+            {link.label}
+          </Navbar.Link>
+        ))}
 
-        <Navbar.Link
-          href="#faqs"
-          className="hover:!text-secundario text-stone-950 text-md font-bold"
-        >
-          FAQS
-        </Navbar.Link>
         <Navbar.Link
           href="#faqs"
           className="hover:!text-secundario text-stone-950 text-md font-bold xl:hidden"
