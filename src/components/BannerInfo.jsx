c1a7ed4b8663c77645b7aac35875f58622a58429
PabloFC <pablofc_68@hotmail.com>
2025-03-22 22:27:07 +0100

    cleaning BannerInfo component

--- src/components/BannerInfo.jsx
+++ src/components/BannerInfo.jsx
@@ -1,39 +1,28 @@
 const BannerInfo = () => {
+  const stats = [
+    { value: "+120K", description: "Usuarios activos mensualmente" },
+    { value: "+300", description: "Canales de señales y traders" },
+    {
+      value: "+25K",
+      description: "Señales han sido enviadas hasta el momento",
+    },
+    {
+      value: "+40",
+      description: "Cursos disponibles y filtrados minuciosamente",
+    },
+  ];
+
   return (
     <section>
       <div className="mx-auto w-full bg-primario py-12 flex flex-col sm:flex-row gap-2 ">
-        <div className="basis-1/4">
-          <h2 className="text-3xl text-center text-secundario font-bold ">
-            +120K
-          </h2>
-          <h5 className="text-center text-white px-8">
-            Usuarios activos mensualmente
-          </h5>
-        </div>
-        <div className="basis-1/4">
-          <h2 className="text-3xl text-center text-secundario font-bold ">
-            +300
-          </h2>
-          <h5 className="text-center text-white px-8">
-            Canales de señales y traders
-          </h5>
-        </div>
-        <div className="basis-1/4">
-          <h2 className="text-3xl text-center text-secundario font-bold ">
-            +25K
-          </h2>
-          <h5 className="text-center text-white px-8">
-            Señales han sido enviadas hasta el momento
-          </h5>
-        </div>
-        <div className="basis-1/4">
-          <h2 className="text-3xl text-center text-secundario font-bold ">
-            +40
-          </h2>
-          <h5 className="text-center text-white px-8">
-            Cursos disponibles y filtrados minuciosamente
-          </h5>
-        </div>
+        {stats.map((stat, index) => (
+          <div key={index} className="basis-1/4">
+            <h2 className="text-3xl text-center text-secundario font-bold">
+              {stat.value}
+            </h2>
+            <h5 className="text-center text-white px-8">{stat.description}</h5>
+          </div>
+        ))}
       </div>
     </section>
   );
