21645b14c45518f665ca71f7abd8b6adf919658a
PabloFC <pablofc_68@hotmail.com>
2025-03-29 21:18:14 +0100

    code cleaned at Blog.jsx component

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,13 @@
-// import grafica from "../assets/img/landing/grafica.png";
+import articles from "../store/dataBlog.json";
 import oro from "../assets/img/landing/oro.png";
 import apple2 from "../assets/img/landing/apple2.jpg";
 import petroleo from "../assets/img/landing/petroleo.jpg";
+
+const images = {
+  oro,
+  apple2,
+  petroleo,
+};
 
 const Blog = () => {
   return (
@@ -10,84 +16,33 @@ const Blog = () => {
         Últimos artículos
       </h2>
       <div className="flex flex-col sm:flex-row mx-auto justify-center">
-        <div className="w-full mb-4 basis-1/2 border-2 border-solid rounded-xl">
-          <div>
-            <img src={oro} className="w-full " alt="grafica" />
-          </div>
-          <div className="mb-2 p-8">
-            <h3 className="text-primario text-2xl font-bold mt-4 mb-4">
-              El precio del Oro apunta a máximos históricos
-            </h3>
-            <p className="text-xl">
-              El precio del Oro se disparó durante la sesión norteamericana
-              previa al fin de semana, ya que el par XAU/USD cotizó por encima
-              de 2.400$, registrando ganancias de más del 1.5% en medio de la
-              subida de los rendimientos de los bonos del Tesoro estadounidense.{" "}
-              <br />
-              El metal sin rendimiento amplió su avance y amenazó con romper el
-              máximo histórico de 2.431$...
-            </p>
-          </div>
-          <div className="flex justify-end mr-4 py-4">
-            <a href="https://www.fxstreet.es/news/el-precio-del-oro-supera-el-nivel-2400-apunta-a-maximos-historicos-202405172003">
-              <button className="bg-secundario px-6 py-2 text-white mt-2 rounded-3xl ">
-                Leer más
-              </button>
-            </a>
-          </div>
-        </div>
-
-        <div className="w-full basis-1/2 xl:ml-8 ">
-          <div className="mb-5 w-full basis-1/2  border-2 border-solid rounded-xl">
-            <div className="mb-4">
+        {articles.articles.map((article) => (
+          <div
+            key={article.id}
+            className="w-full mb-4 basis-1/2 border-2 border-solid rounded-xl"
+          >
+            <div>
               <img
-                src={apple2}
-                className="w-full xl:h-[275px]  "
-                alt="grafica"
+                src={images[article.image]}
+                className="w-full"
+                alt={article.altText}
               />
             </div>
-            <div className="p-5">
-              <h3 className="text-primario text-2xl xl:text-xl font-bold mb-2">
-                Apple nombrada la mejor empresa estadounidense en 2024
+            <div className="mb-2 p-8">
+              <h3 className="text-primario text-2xl font-bold mt-4 mb-4">
+                {article.title}
               </h3>
-              <p className="text-xl xxl:pr-16 xl:text-lg md:pr-0">
-                Apple ha sido nombrada la mejor empresa estadounidense en 2024
-                por su innovación continua, fuerte desempeño financiero y
-                liderazgo en sostenibilidad...
-              </p>
-              <div className="flex justify-end text-secundario text-md  mr-4">
-                <a href="https://www.revistaeyn.com/empresasymanagement/apple-es-la-firma-mas-respetada-en-estados-unidos-KKEN459157">
+              <p className="text-xl">{article.description}</p>
+            </div>
+            <div className="flex justify-end mr-4 py-4">
+              <a href={article.link}>
+                <button className="bg-secundario px-6 py-2 text-white mt-2 rounded-3xl">
                   Leer más
-                </a>
-              </div>
+                </button>
+              </a>
             </div>
           </div>
-
-          <div className="mb-5 w-full basis-1/2  border-2 border-solid rounded-xl">
-            <div className="mb-3">
-              <img
-                src={petroleo}
-                className="w-full xl:h-[275px]"
-                alt="grafica"
-              />
-            </div>
-            <div className="p-5">
-              <h3 className="text-primario text-2xl xl:text-xl font-bold mb-2">
-                El precio del petróleo sube por tensiones en Medio Oriente
-              </h3>
-              <p className="text-xl xl:text-lg xxl:pr-16 md:pr-0">
-                El precio del petróleo crudo subió un 5% hoy debido a las
-                crecientes tensiones geopolíticas en Medio Oriente, que han
-                afectado la oferta mundial...
-              </p>
-              <div className="flex justify-end text-secundario text-md hover:cursor-pointer mr-4">
-                <a href="https://www.mapfream.com/petroleo-alza-tensiones-geopoliticas/">
-                  <button className="mr-12 lg:mr-0 mt-1">Leer más</button>
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
